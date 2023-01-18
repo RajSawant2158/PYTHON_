4030eb46e923f965c8e873e02c748628487b85d7
Rajvardhan Sawant <98679121+RajSawant2158@users.noreply.github.com>
2023-01-18 14:49:58 +0530

    Add files via upload

---
+++ pythonProject5/main.py
@@ -0,0 +1,10 @@
+
+print("Shivani think")
+print("She is not a girl")
+print("She is not so beautiful")
+print("She is too rude with me.")
+print("She always prefer to waste my time.")
+print("She is too busy in talking with her chu* bf")
+print("Only she thinks he is handsome and mature")
+print("They both are too busy that they sometimes forget about college and sit in cafe for hours doing nothing.")
+
---
+++ pythonProject6/main.py
@@ -0,0 +1,7 @@
+import pyttsx3
+import speech_recognition
+
+
+
+
+
