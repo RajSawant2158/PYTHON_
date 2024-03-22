09e6aeb9b8d0b5e0d11c15889d70fd496f1d31cb
Rajvardhan Sawant <98679121+RajSawant2158@users.noreply.github.com>
2024-03-22 11:01:02 +0530

    Add files via upload

---
+++ i Robotics/Home.js
@@ -0,0 +1,69 @@
+
+
+function button1()
+{
+
+    alert ("going on Working Hours")
+    window.location = "WorkingHours.html"; 
+    return false;
+    
+}
+
+function button2()
+{
+
+    alert ("going to Machine Time");
+    window.location = "MachineTime.html"; 
+    return false;
+}
+
+function button3()
+{
+
+    alert ("going to Work Done");
+    window.location = "WorkDone.html"; 
+    return false;
+}
+
+function button4()
+{
+
+    alert ("going to Welding Sector");
+    window.location = "WeldingSector.html"; 
+    return false;
+}
+
+function button5()
+{
+
+    alert ("going to Project Report");
+    window.location = "Project Progress.html"; 
+    return false;
+}
+
+function button6()
+{
+
+    alert ("going to B Sector");
+    window.location = "b.html"; 
+    return false;
+}
+
+function button7()
+{
+
+    alert ("going to C Sector");
+    window.location = "c.html"; 
+    return false;
+}
+
+function button8()
+{
+
+    alert ("going to D Sector");
+    window.location = "d.html"; 
+    return false;
+}
+
+
+
